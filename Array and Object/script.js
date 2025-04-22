f448e56df749bef8927153098d89f5cbe4d18450
akshar51 <aksharparekh401@gmail.com>
2025-04-22 16:48:33 +0530

    done

--- Array and Object/script.js
+++ Array and Object/script.js
@@ -10,8 +10,42 @@
 // console.log(arr)
 
 // SPREAD (MERGE OBJECT AND ARRAY)
-let arr1 = [1,2,3,4]
-let arr2 = [5,5,5,5]
+// let arr1 = [1,2,3,4]
+// let arr2 = [5,5,5,5]
+// let arr = [...arr1,...arr2]
+// console.log(arr)
 
-let arr = [...arr1,...arr2]
-console.log(arr)
+// FILTER (GIVE FILTER VALUE OF ARRAY)
+// let arr = [1,2,3,4,5,6]
+// let array = arr.filter((val)=> val % 2 == 0)
+// console.log(array)
+
+// OBJECT KEY
+// let obj = {
+//     name : "hello",
+//     age : 21
+// }
+// let keyys = Object.keys(obj);
+// console.log(keyys)
+
+// OBJECT VALUE ONLY
+// let obj = {
+//     name : "hello",
+//     age : 21
+// }
+// let keyys = Object.values(obj);
+// console.log(keyys)
+
+// MERGING TWO OBJECT
+// let obj1 = {
+//     1 : "a",
+//     2 : "b",
+//     3 : "c"
+// }
+// let obj2 = {
+//     4 : "a",
+//     5 : "b",
+//     6 : "c"
+// }
+// let obj = {...obj1,...obj2}
+// console.log(obj)
