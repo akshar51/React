3e06f5980fb947ea7575e123b58fd3a99ee2b604
akshar51 <aksharparekh401@gmail.com>
2025-04-22 16:31:42 +0530

    done

--- Array and Object/script.js
+++ Array and Object/script.js
@@ -8,3 +8,10 @@
 // let arr1 = arr.splice(4,1);
 // console.log(arr1);
 // console.log(arr)
+
+// SPREAD (MERGE OBJECT AND ARRAY)
+let arr1 = [1,2,3,4]
+let arr2 = [5,5,5,5]
+
+let arr = [...arr1,...arr2]
+console.log(arr)
