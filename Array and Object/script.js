c9fd7c1a7ea28e96c7fe597aa90fd57d607d5e62
akshar51 <aksharparekh401@gmail.com>
2025-04-22 16:27:19 +0530

    done

---
+++ Array and Object/script.js
@@ -0,0 +1,10 @@
+// SLICE(START,END) GIVE ELEMENT FROM START TO WHERE WE WANT
+// let arr = [1,2,3,4,5,6]
+// let arr1 = arr.slice(0,3);
+// console.log(arr1);
+
+// SPLICE(ELEMENT AT 4, DELETE AFTER 4)
+// let arr = [1,2,3,4,5,6]
+// let arr1 = arr.splice(4,1);
+// console.log(arr1);
+// console.log(arr)
