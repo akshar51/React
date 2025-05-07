89f5fbea1f5b2bb190dcd2e39c9249c4990fc0a9
akshar51 <aksharparekh401@gmail.com>
2025-05-07 20:38:56 +0530

    done

--- Form/src/component/Form.jsx
+++ Form/src/component/Form.jsx
@@ -12,7 +12,9 @@ const Form = () => {
 
     const handleSubmit = (e)=>{
         e.preventDefault()
-        
+        setList([...list,user]);
+        setUser({})
+        console.log(user)
     }
 
   return (
@@ -24,19 +26,64 @@ const Form = () => {
         <h2>Login Form</h2>
       <div className="mb-3">
         <label htmlFor="Username" className="form-label">Username</label>
-        <input type="text" onChange={handleChange} className="form-control" id="username" name='username' />
+        <input type="text" 
+        onChange={handleChange} 
+        className="form-control" 
+        id="username" 
+        name='username'
+        value={user.username || ""} />
       </div>
+
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email</label>
-        <input type="email" onChange={handleChange} className="form-control" id="email" name='email'/>
+        <input type="email" 
+        onChange={handleChange} 
+        className="form-control" 
+        id="email" 
+        name='email'
+        value={user.email || ""}/>
       </div>
+
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password</label>
-        <input type="number" onChange={handleChange} className="form-control" id="password" name='password' />
+        <input type="text" 
+        onChange={handleChange} 
+        className="form-control" 
+        id="password" 
+        name='password'
+        value={user.password || ""} />
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
       </form>
         </div>
+        <table className="table table-bordered mt-5">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              list.map((ele,idx)=>{
+                return(
+                    <tr key={idx}>
+                      <td>{idx+1}</td>
+                      <td>{ele.username}</td>
+                      <td>{ele.email}</td>
+                      <td>{ele.password}</td>
+                      <td>
+                        <button className='btn btn-danger'>Delete</button>
+                      </td>
+                    </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
     </div> 
     </>
