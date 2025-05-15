516055a89a5a6a258d68162fc4249b4130209884
akshar51 <aksharparekh401@gmail.com>
2025-05-15 21:37:03 +0530

    done

--- Form-Revision/src/App.jsx
+++ Form-Revision/src/App.jsx
@@ -4,10 +4,24 @@ const App = () => {
 
   const [employee, setEmployee] = useState({});
   const [empData, setEmpData] = useState([]);
+  const [count, setCount] = useState([]);
+  
   
   
   const handleChange = (e)=>{
     const {name,value,checked} = e.target
+
+    if(name === "count"){
+      let newCount = [...count]
+      if(checked){
+        newCount.push(value)
+      }
+      else{
+        newCount = newCount.filter((item)=> item != value)
+      }
+      setCount(newCount)
+    }
+
     let data = {...employee,[name]:value};
     setEmployee(data);
   }
@@ -16,11 +30,18 @@ const App = () => {
     e.preventDefault();
     let data = [...empData,{...employee,id:Date.now()}];
     setEmpData(data)
+    setEmployee({});
+    setCount([])
   }
 
   const handleDelete = (id)=>{
     let data = empData.filter(val => val.id !== id)
     setEmpData(data);
+  }
+
+  const handleEdit = (id)=>{
+    let data = empData.filter((val,idx)=>val.id === id)[0]
+    setEmployee(data);
   }
 
   return (
@@ -35,7 +56,8 @@ const App = () => {
                   <label htmlFor="ename" className="form-label">Employee Name :</label>
                   <input type="text"
                   onChange={handleChange}
-                  name='ename' 
+                  name='ename'
+                  value={employee.ename || ""} 
                   className="form-control" 
                   id="ename"/>
                 </div>
@@ -44,7 +66,8 @@ const App = () => {
                   <label htmlFor="salary" className="form-label">Salary</label>
                   <input type="text"
                   onChange={handleChange}
-                  name='salary' 
+                  name='salary'
+                  value={employee.salary || ""} 
                   className="form-control" 
                   id="salary" />
                 </div>
@@ -53,21 +76,42 @@ const App = () => {
                   <div className='CheckBox'>
 
                     <div className="form-check form-check-inline">
-                      <input className="form-check-input" type="checkbox" id="checkDefault" />
+                      <input 
+                      className="form-check-input" 
+                      type="checkbox" 
+                      id="checkDefault"
+                      name="count" 
+                      value="first"
+                      onChange={handleChange}
+                      checked={count.includes("first")? true : false}/>
                       <label className="form-check-label" htmlFor="checkDefault">
                         First
                       </label>
                     </div>
 
                     <div className="form-check form-check-inline">
-                      <input className="form-check-input" type="checkbox" id="checkChecked"/>
+                      <input 
+                      className="form-check-input" 
+                      type="checkbox" 
+                      id="checkChecked"
+                      name="count"
+                      value="second"
+                      onChange={handleChange}
+                      checked={count.includes("second")? true : false}/>
                       <label className="form-check-label" htmlFor="checkChecked">
                         Second
                       </label>
                     </div>
 
                     <div className="form-check form-check-inline">
-                      <input className="form-check-input" type="checkbox" id="checkChecked"/>
+                      <input 
+                      className="form-check-input" 
+                      type="checkbox" 
+                      id="checkChecked"
+                      name="count"
+                      value="third"
+                      onChange={handleChange}
+                      checked={count.includes("third")? true : false}/>
                       <label className="form-check-label" htmlFor="checkChecked">
                         Third
                       </label>
@@ -101,10 +145,10 @@ const App = () => {
                           <td>{idx + 1}</td>
                           <td>{ename}</td>
                           <td>{salary}</td>
-                          <td></td>
+                          <td>{val.count.toString()}</td>
                           <td>
                             <button onClick={()=>handleDelete(id)} className='btn btn-danger me-2'>Delete</button>
-                            <button className='btn btn-warning'>Edit</button>
+                            <button onClick={()=>handleEdit(id)} className='btn btn-warning'>Edit</button>
                           </td>
                         </tr>
                       )
